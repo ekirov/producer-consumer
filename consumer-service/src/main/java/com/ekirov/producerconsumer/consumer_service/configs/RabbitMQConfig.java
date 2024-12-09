e7c654c0cc5ead40162df4b75ad5ab7221d943a1
emil-kirov <ekirov6@gmail.com>
2024-12-08 16:46:18 -0800

    fixed bug in order for rabbitmq to process message class

--- consumer-service/src/main/java/com/ekirov/producerconsumer/consumer_service/configs/RabbitMQConfig.java
+++ consumer-service/src/main/java/com/ekirov/producerconsumer/consumer_service/configs/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.ekirov.producerconsumer.consumer_service.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +23,17 @@ public class RabbitMQConfig {
     @Bean
     public Queue responseQueue() {
         return new Queue(rabbitMQProperties.getResponseQueue());
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 }
